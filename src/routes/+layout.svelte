1d83c40a15783adb608292aaedd4b9295c2f4a72
LeshaZholner <ZholnerchikA@gmail.com>
2024-11-25 23:13:18 +0300

    fixed scroll and home page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -24,5 +24,14 @@
     display: grid;
     grid-template-columns: 1fr 6fr;
     height: 100vh;
+    overflow: auto;
+  }
+
+  .layout-menu {
+    position: sticky;
+    height: 100vh;
+    top: 0;
+    left: 0;
+    align-self: start;
   }
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,9 @@
 <script lang="ts">
-  import SetCards from '$lib/components/set-cards.svelte';
-  import type { PageData } from './$types';
-
-  interface Props {
-    data: PageData;
-  }
-
-  let { data }: Props = $props();
 </script>
 
-<div class="card-container">
-  <SetCards cardList={data.cards} />
+<div class="home-page">
+
 </div>
 
 <style lang="scss">
-  .card-container {
-    max-width: 800px;
-    margin-left: auto;
-    margin-right: auto;
-    @include big-font;
-  }
 </style>
