d8953d2360708bb56199ed0af4e70a4ea66e235b
LeshaZholner <ZholnerchikA@gmail.com>
2024-12-14 00:45:56 +0300

    update mylibrary page

--- src/routes/my-library/+page.svelte
+++ src/routes/my-library/+page.svelte
@@ -1,47 +1,25 @@
 <script lang="ts">
   import type { PageData } from './$types';
+  import { Badge } from '$lib/components/ui/badge';
+  import * as Card from '$lib/components/ui/card';
 
   let { data }: { data: PageData } = $props();
 </script>
 
 <div class="my-library-page">
-  <div class="container">
-    <div class="row">
-      <div class="col-12">
-        <div class="dashboard">
-          {#each data.setsPreview as setPreview}
-            <div class="dashboard-item">
-              <div class="card set-preview">
-                <div class="card-body set-preview-inner">
-                  <span class="card-subtitle set-preview-cardLine">
-                    {setPreview.termsCount} terms
-                  </span>
-                  <h5 class="card-title set-preview-cardHeader">{setPreview.title}</h5>
-                  <a
-                    href="/cards/{setPreview.id}"
-                    class="stretched-link"
-                    aria-label={setPreview.title}
-                  ></a>
-                </div>
-              </div>
-            </div>
-          {/each}
-        </div>
-      </div>
-    </div>
+  <div class="flex flex-1 flex-col gap-4">
+    {#each data.setsPreview as setPreview}
+      <Card.Root class="shadow-md transition-all ease-in-out duration-300 hover:shadow-xl">
+        <a href="/cards/{setPreview.id}">
+          <Card.Content class="p-4">
+            <Badge variant="outline">{setPreview.termsCount} terms</Badge>
+            <h4>{setPreview.title}</h4>
+          </Card.Content>
+        </a>
+      </Card.Root>
+    {/each}
   </div>
 </div>
 
-<style lang="scss">
-  .dashboard-item {
-    margin-bottom: 1rem;
-  }
-
-  // .card {
-  //   box-shadow: $box-shadow-sm;
-  // }
-
-  // .card:hover {
-  //   box-shadow: $box-shadow;
-  // }
+<style>
 </style>
